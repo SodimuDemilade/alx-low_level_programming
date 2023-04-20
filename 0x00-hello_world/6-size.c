159cd244dfd989b98c34f58464e7d8bfd3619952
SodimuDemilade <demisodimu@gmail.com>
2023-04-20 16:49:58 -0700

    Size of datatypes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of a char: %zu bytes(s)", sizeof(char));
+	printf("Size of an int: %zu bytes(s)", sizeof(int));
+	printf("Size of a long int: %zu bytes(s)", sizeof(long int));
+	printf("Size of a long long int: %zu bytes(s)", sizeof(long long int));
+	printf("Size of a float: %zu bytes(s)", sizeof(float));
+	return (0);
+}
