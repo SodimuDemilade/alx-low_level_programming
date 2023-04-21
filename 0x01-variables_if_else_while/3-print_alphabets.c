55a64e16a13084649d2d7f9f4c8924a26a2b4755
SodimuDemilade <demisodimu@gmail.com>
2023-04-21 15:44:42 -0700

    For loop in C

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for (int i = 'a' ; i <= 'z' ; i++)
+	{
+		putchar(i);
+	}
+	for (int i = 'A' ; i <= 'Z' ; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
