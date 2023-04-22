d42ff9a87a85ebd1495e3874cb64d4ef3355b071
SodimuDemilade <demisodimu@gmail.com>
2023-04-21 18:00:31 -0700

    For loops in C

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = '0' ; i <= '9' ; i++)
+	{
+		for (j = '0' ; j <= '9'; j++)
+		{
+			putchar(i);
+			putchar(j);
+			putchar(' ');
+			for (k = '0' ; k <= '9' ; k++)
+			{
+				for (l = '0' ; l <= '9' ; l++)
+				{
+					if (l > k)
+					{
+						putchar(k);
+						putchar(l);
+						if (j != '8')
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
