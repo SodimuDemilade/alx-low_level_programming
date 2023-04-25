13f73d9e89048343b84ffe1a13d60e1e7b003415
SodimuDemilade <demisodimu@gmail.com>
2023-04-25 14:47:17 -0700

    x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - check the code.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 'a';
+	int count = 0;
+	while (count < 9)
+	{
+		while (i <= 'z')
+		{
+			putchar(i);
+			i++;
+		}
+		count++;
+	}
+	putchar('\n');
+}
