c1aee0fff2c18833496bbc9240c55c274a00d197
SodimuDemilade <demisodimu@gmail.com>
2023-04-25 12:49:55 -0700

    alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -30,4 +30,16 @@ int _putchar(void)
         putchar(letter);
 	return (0);
 }
+
+int print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
 #endif
