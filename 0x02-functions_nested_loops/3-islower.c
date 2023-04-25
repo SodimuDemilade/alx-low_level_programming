3b0031f2b7c17da77c5b36f8d163fc7316a60845
SodimuDemilade <demisodimu@gmail.com>
2023-04-25 15:02:36 -0700

    Lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * _islower - check the code.
+ *
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+	if (islower(c) == '2')
+	{
+		return '0';
+	}
+	else
+	{
+		return '1';
+	}
+}
