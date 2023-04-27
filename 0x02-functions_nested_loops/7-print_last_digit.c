d056c4543e216407cb939838ba9eaad799bafd21
SodimuDemilade <demisodimu@gmail.com>
2023-04-27 09:47:12 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_last_digit - check the code.
+ *
+ * @c: check the parameter.
+ *
+ * Return: Always 0.
+ */
+
+int print_last_digit(int c)
+{
+	return (c%10);
+}
