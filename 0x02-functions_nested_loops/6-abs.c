0c848304caf82ac58ea0447395b04babfd8afeef
SodimuDemilade <demisodimu@gmail.com>
2023-04-27 09:42:41 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _abs - check the code.
+ *
+ * @c: check the parameter.
+ *
+ * Return: Always 0.
+ */
+
+int _abs(int c)
+{
+	return (abs(c));
+}
