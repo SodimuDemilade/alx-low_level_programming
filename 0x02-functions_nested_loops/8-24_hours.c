09204b25828ed71de265b4df67b8932e982fcd6e
SodimuDemilade <demisodimu@gmail.com>
2023-04-27 10:08:43 -0700

    Hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,13 +11,13 @@ void jack_bauer(void)
 {
 	int i, j, k, l;
 
-	for (i = 0 ; i <= 2 ; i++)
+	for (i = '0' ; i <= '2' ; i++)
 	{
-		for (j = 0 ; j <= 3 ; j++)
+		for (j = '0' ; j <= '3' ; j++)
 		{
-			for (k = 0 ; k <= 5 ; k++)
+			for (k = '0' ; k <= '5' ; k++)
 			{
-				for (l = 0 ; l <= 9 ; l++)
+				for (l = '0' ; l <= '9' ; l++)
 				{
 					putchar(i);
 					putchar(j);
