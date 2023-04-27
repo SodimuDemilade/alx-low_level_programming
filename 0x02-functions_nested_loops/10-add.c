179c5b16807efa57574bd171f2f4b81604f1f99b
SodimuDemilade <demisodimu@gmail.com>
2023-04-27 11:44:24 -0700

    Add 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - Entry point
+ *
+ * @a: check parameter.
+ *
+ * @b: check parameter.
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int add(int a, int b)
+{
+	int c;
+	c = a + b;
+	return (c);
+}
