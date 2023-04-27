715b4d4585f47fd06988bf3788544116c667ac09
SodimuDemilade <demisodimu@gmail.com>
2023-04-27 10:36:59 -0700

    times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,16 +11,16 @@
 
 void times_table(void)
 {
-	int i, j;
+	int i, j, a;
 
-	for (i = '0' ; i <= '9' ; i++)
+	for (i = 0 ; i <= 9 ; i++)
 	{
-		for (j = '0' ; j <= '9' ; j++)
+		for (j = 0 ; j <= 9 ; j++)
 		{
-			int a = i * j;
+			a = i * j;
 			putchar(a);
 			putchar(',');
-			putchar(':');
+			putchar(' ');
 		}
 		putchar('\n');
 	}
