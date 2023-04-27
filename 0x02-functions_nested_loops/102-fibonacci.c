4b7f00edec481988a2a060be19f7c67c8485d6a3
SodimuDemilade <demisodimu@gmail.com>
2023-04-27 13:32:17 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int count = 0;
+	int a = 1;
+	int b = 2;
+	int c;
+
+	printf("%d, %d", a, b);
+	while (count < 50)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		printf(", %d, %d", a, b);
+		count++;
+	}
+}
