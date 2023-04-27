5bbff584996ad9225d68e7ddce022dada4228989
SodimuDemilade <demisodimu@gmail.com>
2023-04-27 08:42:40 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * _putchar : test the function.
+ * main - Entry point
  *
  * Description: 'the program's description'
  *
@@ -11,21 +11,23 @@
 int main(void)
 {
 	int letter;
-        letter = '_';
-        putchar(letter);
-        letter = 'p';
-        putchar(letter);
-        letter = 'u';
-        putchar(letter);
-        letter = 't';
-        putchar(letter);
-        letter = 'c';
-        putchar(letter);
-        letter = 'h';
-        putchar(letter);
-        letter = 'a';
-        putchar(letter);
-        letter = 'r';
-        putchar(letter);
-        return (0);
+
+	letter = '_';
+	putchar(letter);
+	letter = 'p';
+	putchar(letter);
+	letter = 'u';
+	putchar(letter);
+	letter = 't';
+	putchar(letter);
+	letter = 'c';
+	putchar(letter);
+	letter = 'h';
+	putchar(letter);
+	letter = 'a';
+	putchar(letter);
+	letter = 'r';
+	putchar(letter);
+	putchar('\n');
+	return (0);
 }
