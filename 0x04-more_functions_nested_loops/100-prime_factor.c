8acf6cc263a1459453fe08b876bc10d2cdc15131
SodimuDemilade <demisodimu@gmail.com>
2023-04-28 11:56:54 -0700

    functions

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+{
+	long int i;
+
+	for (i = 2 ; i < 612852475143 ; i++)
+	{
+		if (i 
+	}	
+}
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - check the code.
+ *
+ * @n: check the parameter.
+ *
+ */
+
+void print_number(int n)
+{
+	int reverse = 0;
+	
+	if (n == 0)
+		putchar('0');
+	while (n != 0)
+	{
+		reverse = reverse * 10;
+		reverse = reverse + n % 10;
+		n = n / 10;
+	}
+	while (reverse != 0)
+	{	
+		putchar((reverse % 10) + '0');
+		reverse = reverse / 10;
+	}
+}
