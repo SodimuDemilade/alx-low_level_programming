d24c555842f6aaadcdf8208db39510e255d8a0f6
SodimuDemilade <demisodimu@gmail.com>
2023-04-28 07:26:29 -0700

    functions

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - check the code.
+ *
+ */
+
+void more_numbers(void)
+{
+	int i, a;
+	int count = 0;
+	
+	while (count < 10)
+	{
+	for (i = 0 ; i <= 14 ; i++)
+	{
+		if (i > 9)
+		{
+			putchar((i / 10) + '0');
+		}
+		putchar((i % 10) + '0');
+	}
+	count++;
+	}
+	putchar('\n');
+}
