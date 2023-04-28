149c00aa48294aa7629e6a7d85ea861a1602a101
SodimuDemilade <demisodimu@gmail.com>
2023-04-28 07:00:28 -0700

    functions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - check the code.
+ *
+ * @a: check the parameter.
+ *
+ * @b: check the parameter.
+ *
+ * Return: check the code.
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
