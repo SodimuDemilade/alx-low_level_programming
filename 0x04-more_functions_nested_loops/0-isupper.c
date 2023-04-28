9147bd2a748dd16fc687b55be392b67afd66b1f3
SodimuDemilade <demisodimu@gmail.com>
2023-04-28 06:44:36 -0700

    functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isupper - check the code.
+ *
+ * @c: check the parameter.
+ *
+ * Return : Always 0.
+ */
+
+int _isupper(int c)
+{
+	if (isupper(c) == 0)
+		return (0);
+	else
+		return (1);
+}
