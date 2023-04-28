a03a7288f197187cd65bb03a26e26f66c57aaaad
SodimuDemilade <demisodimu@gmail.com>
2023-04-28 07:04:47 -0700

    functions

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - check the code.
+ *
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0' ; i <= '9' ; i++)
+		putchar(i);
+	putchar('\n');
+}
