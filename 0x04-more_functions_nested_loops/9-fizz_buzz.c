2e9aa7872c45af017e3d7e7261dfdd859cc5e86e
SodimuDemilade <demisodimu@gmail.com>
2023-04-28 10:05:18 -0700

    functions

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,22 +13,31 @@ int main(void)
 
 	for (i = 1 ; i <= 100 ; i++)
 	{
+		if (i != 100)
+		{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
+			putchar(' ');
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz");
+			putchar(' ');
 		}
 		else if (i % 3 == 0)
 		{
 			printf("Fizz");
+			putchar(' ');
 		}
 		else
 		{
 			printf("%d", i);
+			putchar(' ');
 		}
+		}
+		else
+			printf("Buzz");
 	}
 	putchar('\n');
 	return (0);
