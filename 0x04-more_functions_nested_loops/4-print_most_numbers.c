21e1b274db874ff532cf41a93317d48f0b3571bf
SodimuDemilade <demisodimu@gmail.com>
2023-04-28 07:11:33 -0700

    functions

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - check the code.
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0' ; i <= '9' ; i++)
+	{
+		if (i == '2' || i == '4')
+			break;
+		putchar(i);
+	}
+	putchar('\n');
+}
