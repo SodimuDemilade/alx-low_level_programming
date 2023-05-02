b03eb16c005ffcd783b104b98903205f6f045123
SodimuDemilade <demisodimu@gmail.com>
2023-05-02 12:56:29 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - check the code.
+ *
+ * @s: check the parameter.
+ *
+ * Return: check the code.
+ *
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
