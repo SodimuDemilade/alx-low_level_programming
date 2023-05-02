30eb949e4864cd477232956b1ed5261b40fcded4
SodimuDemilade <demisodimu@gmail.com>
2023-05-02 14:58:46 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - check the code.
+ *
+ * @dest: check the parameter.
+ *
+ * @src: check the parameter.
+ *
+ * Return: check the code.
+ *
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (*dest);
+}
