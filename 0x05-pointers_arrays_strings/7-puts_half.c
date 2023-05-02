1f3f0c78431a705dcd92a1a432ca6ff88a663748
SodimuDemilade <demisodimu@gmail.com>
2023-05-02 14:36:44 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts_half - check the code.
+ *
+ * @str: check the parameter.
+ *
+ */
+
+void puts_half(char *str)
+{
+	unsigned long int i, length, n;
+
+	length = strlen(str);
+	if (length % 2 == 0)
+		n = length / 2;
+	else
+		n = (length - 1) / 2;
+	for (i = n ; i < length ; i++)
+	{
+		putchar(str[n]);
+	}
+	putchar('\n');
+}
