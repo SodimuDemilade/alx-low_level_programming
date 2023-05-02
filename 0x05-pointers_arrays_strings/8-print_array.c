747331bb815b13af51ef0c48ee7b51a428154a0f
SodimuDemilade <demisodimu@gmail.com>
2023-05-02 14:52:14 -0700

    pointer

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -17,8 +17,11 @@ void print_array(int *a, int n)
 	for (i = 0 ; i < n ; i++)
 	{
 		printf("%d", a[i]);
-		putchar(',');
-		putchar(' ');
+		if (i != (n - 1))
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 }
