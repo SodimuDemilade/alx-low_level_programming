1be1a2c051966557a408b117e0f3dd3cd242a34f
SodimuDemilade <demisodimu@gmail.com>
2023-05-02 13:18:29 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev - check the code.
+ *
+ * @s: check the parameter.
+ *
+ */
+
+void print_rev(char *s)
+{
+	printf("%s", strrev(s));
+}
