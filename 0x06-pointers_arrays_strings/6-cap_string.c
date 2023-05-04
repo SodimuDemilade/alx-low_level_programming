c567287efd97af5e342bb1064d9d3a377a71c788
SodimuDemilade <demisodimu@gmail.com>
2023-05-04 08:43:20 -0700

    pointers

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+/*
+ * cap_string - check the code.
+ *
+ * Description: something
+ *
+ * @str: check the parameter.
+ *
+ * Return: check the code.
+ */
+
+char *cap_string(char *str)
+{
+	unsigned long int i, j;
+	char arr[12] = {',', ';', '.', '?', '"', '(', ')', '{', '}', '\n', ' ', '	'};
+
+	for (i = 0 ; i < strlen(str) ; i++)
+	{
+		for (j = 0; j < strlen(arr) ; j++)
+		{
+			if (str[i] == arr[j])
+			{
+				str[i + 1] = toupper(str[i + 1]);
+			}
+		}
+	}
+	return (str);
+}
