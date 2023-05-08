6d4566a9e95553e0c36ba951bf250c74222f3eed
SodimuDemilade <demisodimu@gmail.com>
2023-05-08 13:15:58 -0700

    Pointers and arrays

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,11 +10,11 @@
 
 void print_chessboard(char (*a)[8])
 {
-	unsigned long int i, j;
+	int i, j;
 
-	for (i = 0 ; i < strlen(a) ; i++)
+	for (i = 0 ; i < 8 ; i++)
 	{
-		for (j = 0 ; j < strlen(a) ; j++)
+		for (j = 0 ; j < 8 ; j++)
 		{
 			if (a[i][j] == ' ')
 				continue;
