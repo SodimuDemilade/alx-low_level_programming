c43e52a9023a6381439f2135def1da584818b67e
SodimuDemilade <demisodimu@gmail.com>
2023-05-08 12:34:01 -0700

    Pointers and arrays

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strchr - check the code.
+ *
+ * @s: check the parameters.
+ *
+ * @c: check the parameters.
+ *
+ * Return: check the code.
+ */
+
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
