bed8eeb227b4e46ce1ca8910823a2aefa767a57f
SodimuDemilade <demisodimu@gmail.com>
2023-05-16 14:14:07 -0700

    malloc

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -15,11 +15,7 @@ void print_number(int n)
 		putchar('-');
 		n = -n;
 	}
-
-    /* Remove the last digit and recur*/
 	if (n / 10)
 		print_number(n / 10);
-
-    /* Print the last digit*/
 	putchar(n % 10 + '0');
 }
