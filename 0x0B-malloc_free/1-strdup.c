2a36fd0e0174539ba17dccec704175571f720dc9
SodimuDemilade <demisodimu@gmail.com>
2023-05-16 14:56:50 -0700

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -19,7 +19,7 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	nstr = malloc(sizeof(char) * strlen(str));
+	nstr = malloc(sizeof(char) * (strlen(str) + 1));
 	if (nstr == NULL)
 		return (NULL);
 	for (i = 0 ; i < strlen(str) ; i++)
