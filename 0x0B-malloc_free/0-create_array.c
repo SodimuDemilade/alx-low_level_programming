acdb801860d84e5c87e62ed70f546ca322dd4872
SodimuDemilade <demisodimu@gmail.com>
2023-05-16 14:10:22 -0700

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,6 +17,8 @@ char *create_array(unsigned int size, char c)
 	char *myarr;
 	unsigned int i;
 
+	if (size == 0)
+		return (NULL);
 	myarr = malloc(sizeof(char) * size);
 	for (i = 0 ; i < size ; i++)
 	{
