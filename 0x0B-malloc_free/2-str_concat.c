a2414ba06cc040c20c7475ded7443821a93c00eb
SodimuDemilade <demisodimu@gmail.com>
2023-05-16 15:43:48 -0700

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,9 +19,9 @@ char *str_concat(char *s1, char *s2)
 	unsigned int tlen;
 
 	if (s1 == NULL)
-		s1 = " ";
+		s1 = "";
 	if (s2 == NULL)
-		s2 = " ";
+		s2 = "";
 	tlen = strlen(s1) + strlen(s2);
 	nstr = malloc(sizeof(char) * (tlen + 1));
 	if (nstr == NULL)
