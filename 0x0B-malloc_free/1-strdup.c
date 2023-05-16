bc144ddf2b1f928f10ea395edc686742f45ca32c
SodimuDemilade <demisodimu@gmail.com>
2023-05-16 14:21:48 -0700

    malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strdup - check the code.
+ *
+ * @str: check the parameter.
+ *
+ * Return: check the code.
+ *
+ */
+
+char *_strdup(char *str)
+{
+	if (str == NULL)
+		return (NULL);
+	nstr = malloc(sizeof(char) * strlen(str));
+}
