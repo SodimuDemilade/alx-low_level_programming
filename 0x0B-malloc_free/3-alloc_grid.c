20a6e18591361687e4e67f51c416c09d7271a3a1
SodimuDemilade <demisodimu@gmail.com>
2023-05-17 16:20:14 -0700

    malloc

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - check the code.
+ *
+ * @width: check the parameters.
+ *
+ * @height: check the parameters.
+ *
+ * Return: check the code.
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **arr;
+	unsigned int i;
+
+	if (width <= 0 || height <= 0)
+		return NULL;
+	arr = malloc(sizeof(int) * (width * height));
+	if (arr == NULL)
+		return NULL;
+	for (i = 0 ; i < strlen(arr) ; i++)
+		arr[i] = 0;
+	return (arr);
+}
