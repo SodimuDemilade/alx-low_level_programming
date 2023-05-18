90a05df48fd607dffef900db2240cec2b0b70da2
SodimuDemilade <demisodimu@gmail.com>
2023-05-18 03:33:45 -0700

    malloc

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * argstostr - check the code.
+ *
+ * @ac: check the parameters.
+ *
+ * @av: check the parameters.
+ *
+ * Return: check the code.
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char "nstr;
+	unsigned int tlen;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	tlen = strlen(av) + strlen(ac);
+	nstr = malloc(sizeof(char) * tlen);
+	if (nstr == NULL)
+		return (NULL);
+	strcpy(nstr, av);
+	strcat(nstr, ac);
+	return (nstr);
+}
