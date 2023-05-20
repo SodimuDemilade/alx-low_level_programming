0300102f8524ed98949d62d17da5e7e40f714d0e
SodimuDemilade <demisodimu@gmail.com>
2023-05-20 04:48:56 -0700

    arg

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,13 +2,20 @@
 
 /**
  * main - check the code"
+ *
+ * @argc: check the parameters.
+ *
+ * @argv: check the parameters.
+ *
+ * Return: check the code.
+ *
  */
 
 int main(int argc, char *argv[])
 {
 	if (argc == 1)
 	{
-	printf ("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	}
 	return (0);
 }
