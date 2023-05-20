82faee4b5b3dece2ca372059393587158ffed321
SodimuDemilade <demisodimu@gmail.com>
2023-05-20 09:33:07 -0700

    arg

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code.
+ *
+ * @argv: check the parameters.
+ *
+ * @argc: check the parameters.
+ *
+ * Return: check the code.
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int add = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1 ; i < argc ; i++)
+		{
+			if (atoi(argv[i]) == 0)
+			{
+			       printf("Error\n");
+			       return (1);
+			}
+			add += atoi(argv[i]);
+		}
+		printf("%d\n", add);
+		return (0);
+	}
+	else
+        {
+                printf("%d\n", 0);
+                return (1);
+        }
+}
