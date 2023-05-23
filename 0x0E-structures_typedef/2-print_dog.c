140f49d4b0647b1cc455d63f6a8db6e706f25c0e
SodimuDemilade <demisodimu@gmail.com>
2023-05-23 14:22:19 -0700

    structures

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,13 +12,13 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		printf("Nothing");
-	if (d -> name == NULL)
+	if (d->name == NULL)
 		printf("Name: (nil)");
 	else
 		printf("Name: %s\n", d -> name);
 	printf("Age: %f\n", d -> age);
-	if (d -> owner == NULL)
-		printf("Name: (nil)");
+	if (d->owner == NULL)
+		printf("Owner: (nil)");
 	else
 		printf("Owner: %s\n", d -> owner);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,6 +16,7 @@ struct dog
 	float age;
 	char *owner;
 };
-void init_dog(struct dog *d, char *name, float age, char *owner);
+
 void print_dog(struct dog *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif /* ifndef _DOG_H_ */
