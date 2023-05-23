71ffdbe6ac12056740f60c271dccecf3d40fb1d7
SodimuDemilade <demisodimu@gmail.com>
2023-05-23 14:34:32 -0700

    structures

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,6 +15,8 @@ void print_dog(struct dog *d)
 		exit(-1);
 	if (d->name == NULL)
 		printf("Name: (nil)");
+	if (d->age == 0)
+		printf("Age: (nil)");
 	else
 		printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
