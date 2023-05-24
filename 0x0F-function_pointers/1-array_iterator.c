6f877c2d2ae5d96f721d34ace461fd55ba970f67
SodimuDemilade <demisodimu@gmail.com>
2023-05-24 11:21:03 -0700

    function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,2 +1,23 @@
 #include "function_pointers.h"
 #include <stdio.h>
+#include <stddef.h>
+
+/**
+ * array_iterator - check the code.
+ *
+ * @array: check the parameters.
+ *
+ * @size: check the parameters.
+ *
+ * @action: check the parameters.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for(i = 0 ; i < size ; i++)
+	{
+		action(array[i]);
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,5 +2,4 @@
 #define _FUNCTION_H_
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
-void array_iterator(int *array, size_t size, void (*action)(int));
 #endif /*ifndef _FUNCTION_H_*/
