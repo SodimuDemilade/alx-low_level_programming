74b2d1c1885178aa997a794bd269c69f74fd7686
SodimuDemilade <demisodimu@gmail.com>
2023-05-24 02:38:25 -0700

    structures

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * init_dog - check the code.
@@ -16,7 +17,9 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
+	if (d == NULL)
+		printf("OK");
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
