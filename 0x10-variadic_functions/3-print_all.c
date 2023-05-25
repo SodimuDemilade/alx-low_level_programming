74b28390a886ad69df74c7ec289b138ce995b9e7
SodimuDemilade <demisodimu@gmail.com>
2023-05-25 15:18:39 -0700

    functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include "variadic_functions.h"
 
 /**
  * print_all - check the code.
@@ -15,8 +16,46 @@ void print_all(const char * const format, ...)
 	float f;
 	char *s;
 	va_list mylist;
+	int index = 0;
 
 	va_start(mylist, format);
-
-
+	while(format[index] != '\0')
+	{
+		if(format[index] == 'c')
+		{
+			c = (char)va_arg(mylist, int);
+			printf("%c", c);
+			putchar(',');
+			putchar(' ');
+		}
+		else if(format[index] == 'i')
+		{
+			i = va_arg(mylist, int);
+			printf("%i", i);
+			putchar(',');
+			putchar(' ');
+		}
+		else if (format[index] == 'f')
+		{
+			f = (float)va_arg(mylist, double);
+			printf("%f", f);
+			putchar(',');
+			putchar(' ');
+		}
+		else if (format[index] == 's')
+		{
+			s = va_arg(mylist, char *);
+			if (s != NULL)
+			{
+				printf("%s", s);
+			}
+			else
+				printf("(nil)");
+			putchar(',');
+			putchar(' ');
+		}
+		index++;
+	}
+	va_end(mylist);
+	printf("\n");
 }
