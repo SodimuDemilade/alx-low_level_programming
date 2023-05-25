459be3eec56d0ef0f2254f246e2d94a72abb6b2e
SodimuDemilade <demisodimu@gmail.com>
2023-05-25 13:43:21 -0700

    functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -22,10 +22,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0 ; i < n ; i++)
 	{
 		printf("%d", va_arg(mylist, int));
-		if (i != (n - 1))
+		if (i != (n - 1) && separator != NULL)
 		{
-			if (separator != NULL)
-				printf("%s", separator);
+			printf("%s", separator);
 		}
 	}
 	printf("\n");
