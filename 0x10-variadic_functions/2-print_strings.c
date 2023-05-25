72093b1de24a74f9d90596a423765712e495d497
SodimuDemilade <demisodimu@gmail.com>
2023-05-25 12:21:02 -0700

    functions

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - prints strings.
+ *
+ * @separator: separator.
+ *
+ * @n: n.
+ *
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list mylist;
+
+	va_start(mylist, n);
+	for (i = 0 ; i < n ; i++)
+	{
+		if (va_arg(mylist, char*) == NULL)
+			printf("(nil)");
+		else
+		{
+			printf("%s", va_arg(mylist, char*));
+			if (i != (n - 1))
+			{
+				if (separator != NULL)
+				{
+					printf("%s", separator);
+				}
+			}
+		}
+	}
+	printf("\n");
+	va_end(mylist);
+}
