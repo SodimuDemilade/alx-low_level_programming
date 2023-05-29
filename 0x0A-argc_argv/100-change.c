46294f0d26622e5fb71eb03e9ca851dad34e7403
SodimuDemilade <demisodimu@gmail.com>
2023-05-29 13:23:52 -0700

    argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -22,12 +22,12 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	if (atoi(argv[1]) < 0)
-	{
-		printf("%d\n", 0);
-	}
 	num = atoi(argv[1]);
-	for (i = 0 ; i < 5 ; i++)
+	if (num < 0)
+	{
+		printf("0\n");
+	}
+	for (i = 0 ; i < 5 && num >= 0 ; i++)
 	{
 		while (num >= coins[i])
 		{
