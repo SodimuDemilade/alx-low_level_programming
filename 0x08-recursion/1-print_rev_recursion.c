0487fbe29e814443f870774316bcc5ff9d797aa7
SodimuDemilade <demisodimu@gmail.com>
2023-05-29 12:47:09 -0700

    recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,21 +1,17 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
  * _print_rev_recursion - check the code.
  *
- * @s: check the parameter.
+ * @s: parameter passed.
  *
  */
 
 void _print_rev_recursion(char *s)
 {
-	int num;
-
-	num = atoi(s);
-	if (num == 0)
-		return;
-	putchar(num % 10);
-	_print_rev_recursion(num / 10);
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
