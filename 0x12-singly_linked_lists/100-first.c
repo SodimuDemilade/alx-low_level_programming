2a78f76d1b253c7dc13ca9685309511214958782
SodimuDemilade <demisodimu@gmail.com>
2023-06-01 17:04:59 -0700

    linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * myprint - check the code.
+ *
+ * @str: check the parameter.
+ *
+ */
+
+void myprint()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
