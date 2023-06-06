36e1cd02ec0080c507a95f0d140282ca62cc4e96
SodimuDemilade <demisodimu@gmail.com>
2023-06-06 01:43:27 -0700

    malloc

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,6 +2,32 @@
 #include <ctype.h>
 #include <stdlib.h>
 
+int check_number(char *str);
+
+/**
+ * check_number - check the code.
+ *
+ * @str: check the parameter.
+ *
+ * Return: check the code,
+ *
+ */
+
+int check_number(char *str)
+{
+	unsigned int count = 0;
+
+	while (count < strlen(str))
+	{
+		if (!isdigit(str[count]))
+		{
+			return (0);
+		}
+		count++;
+	}
+	return (1);
+}
+
 /**
  * main - check the code.
  *
@@ -16,25 +42,22 @@
 int main(int argc, char *argv[])
 {
 	int i;
+	int j;
 	int add = 0;
 
-	if (argc > 1)
-	{
 		for (i = 1 ; i < argc ; i++)
 		{
-			if (isdigit(*argv[i]) == 0)
+			if (check_number(argv[i]))
+			{
+				j = atoi(argv[count]);
+				add += j
+			}
+			else
 			{
 				printf("Error\n");
 				return (1);
 			}
-			add += atoi(argv[i]);
 		}
 		printf("%d\n", add);
 		return (0);
-	}
-	else
-	{
-		printf("%d\n", 0);
-		return (1);
-	}
 }
