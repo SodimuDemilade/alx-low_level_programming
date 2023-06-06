43a68d96c394de7f1617668c768801adef8e3375
SodimuDemilade <demisodimu@gmail.com>
2023-06-06 10:41:03 -0700

    lists

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,7 +14,7 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned count = 0;
+	unsigned int count = 0;
 	listint_t *var = head;
 
 	while (var && count < index)
--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,4 +1,26 @@
 #include "lists.h"
 
 /**
+ * sum_listint - check the code;
  *
+ * @head: check the parameter.
+ *
+ * Return: check the code.
+ *
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *currnode = head;
+	
+	if (currnode == NULL)
+		return (0);
+	while (currnode != NULL)
+	{
+		sum += currnode->n;
+		currnode = currnode->next;
+
+	}
+	return (sum);
+}
