8ce59d5d740ea468e289f7d836b03c055f4ab442
SodimuDemilade <demisodimu@gmail.com>
2023-06-06 05:56:55 -0700

    lists

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * listint_len - check the code.
+ *
+ * @h: check the parameter.
+ *
+ * Return: check the code.
+ *
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+	while (h != NULL)
+	{
+	count++;
+	h = h->next;
+	}
+	return (count);
+}
