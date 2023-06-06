3dbf6c1704c8162143e006db9deab9e06a596e79
SodimuDemilade <demisodimu@gmail.com>
2023-06-06 01:24:32 -0700

    malloc

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int _atoi(char *s);
+
+/**
+ * _atoi - check the code.
+ *
+ * @a: check the parameter.
+ *
+ * Return: check the code.
+ *
+ */
+
+int _atoi(char *a)
+{
+	int i = 0, j = 0, k = 0, count = 0, l = 0, num = 0;
+
+	while (i < count && l == 0)
+	{
+		if (a[i] == '-')
+			++j;
+		if (a[i] >= '0' && a[i] <= '9')
+		{
+			num = a[i] - '0';
+			if (j % 2)
+				num = -num;
+			k = k * 10 + digit;
+			l = 1;
+			if (a[i + 1] < '0' || a[i + 1] > '9')
+				break;
+			l = 0;
+		}
+		i++;
+	}
+	if (l == 0)
+		return (0);
+	return (k);
+}
+
 /**
  * main - check the code.
  *
@@ -14,11 +51,13 @@
 
 int main(int argc, char *argv[])
 {
-	int result;
+	int result, a, b;
 
-	if (argc > 1)
+	if (argc == 3)
 	{
-		result = atoi(argv[1]) * atoi(argv[2]);
+		a = _atoi(argv[1]);
+		b = _atoi(argv[2]);
+		result = a * b;
 		printf("%d\n", result);
 	}
 	else
