61679e1b80e2c35f1b55830b98a9ca57f4923cfd
SodimuDemilade <demisodimu@gmail.com>
2023-06-06 00:40:47 -0700

    malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -26,10 +26,15 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 	for (i = 0 ; i < height ; i++)
-		arr[i] = (int *)malloc(width * sizeof(int));
-	if (arr == NULL)
 	{
-		return (NULL);
+		arr[i] = (int *)malloc(width * sizeof(int));
+		if (arr[i] == NULL)
+		{
+			for (; i >= 0 ; i--)
+				free(arr[i]);
+			free(arr);
+			return (NULL);
+		}
 	}
 	for (i = 0 ; i < height ; i++)
 	{
