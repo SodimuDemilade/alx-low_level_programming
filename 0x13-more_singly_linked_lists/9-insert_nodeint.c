e23d36db3871b89d70b1cfb34c3dd7e74a69eb44
SodimuDemilade <demisodimu@gmail.com>
2023-06-06 10:56:19 -0700

    lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,5 +15,31 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	unsigned int count;
+	listint_t *var;
+	listint_t *value = *head;
 
+	var = malloc(sizeof(listint_t));
+	if (!var || !head)
+		return (NULL);
+	var->n = n;
+	var->next = NULL;
+	if (idx == 0)
+	{
+		var->next = *head;
+		*head = var;
+		return (var);
+	}
+	for (count = 0 ; value && count < idx ; i++)
+	{
+		if (count == idx - 1)
+		{
+			var->next = value->next;
+			value->next = var;
+			return (var);
+		}
+		else
+			value = value->next;
+	}
+	return (NULL);
 }
