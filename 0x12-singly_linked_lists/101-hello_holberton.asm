37b77c75f6ed73bd7061e8de1d91546024c4d7cc
SodimuDemilade <demisodimu@gmail.com>
2023-06-06 21:43:10 -0700

    hello

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,11 @@
-section .data
-	myvar db 'Hello, Holberton', 10
-	format db "%s", 0
+global main
+extern printf
 
-section .text
-	global _start
+main:
+mov edi, format
+xor eax, eax
+call printf
+mov eax, 0
+ret
 
-_start:
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, myvar
-	mov rdx, 13
-	syscall
-	
-	mov rax, 60
-	mov rdi, 0
-	syscall
+format: db "Hello, Holberton\n", 0
---
+++ 0x12-singly_linked_lists/101-hello_holberton.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int myfunc();
+
+int main()
+{
+	myfunc();
+	return 0;
+}
