1bc73110ea492b93535b4aa93aa889ad539e5e72
SodimuDemilade <demisodimu@gmail.com>
2023-08-02 14:31:05 -0700

    stuff

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - main
+ *
+ * @s: check
+ *
+ * return: check
+ *
+ */
+
+int _strlen_recursion(char *s)
+{
+	int count = 0;
+	if (*s)
+	{
+		count++;
+		count += _strlen_recursion(s + 1);
+	}
+	return (count);
+}
