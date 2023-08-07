514042f816898d63edc6b58ab07b05d3d0ea04fb
SodimuDemilade <demisodimu@gmail.com>
2023-08-07 07:20:40 -0700

    arg

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - main
+ *
+ * @argv: check
+ *
+ * @argc: check
+ *
+ * Return: check
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0 ; i < argc ; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
