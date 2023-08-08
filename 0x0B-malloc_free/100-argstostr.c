ba9025b07c77b34dc8c8da77baeaa082106f7576
SodimuDemilade <demisodimu@gmail.com>
2023-08-08 14:38:28 -0700

    malloc

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * argstostr - main
+ *
+ * @ac: check
+ *
+ * @av: check
+ *
+ * Return: check
+ *
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *b;
+	int i, total = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0 ; i < ac ; i++)
+	{
+		total += strlen(av[i]) + 1;
+	}
+	b = malloc(sizeof(char) * total);
+	if (b == NULL)
+		return (NULL);
+	for (i = 0 ; i < ac ; i++)
+	{
+		strcat(b, av[i]);
+		strcat(b, "\n");
+	}
+	return (b);
+}
