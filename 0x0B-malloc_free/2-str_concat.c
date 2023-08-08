e3dd99a0ae5ecea5d533f5f72578e24b0898a74b
SodimuDemilade <demisodimu@gmail.com>
2023-08-08 13:48:29 -0700

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,10 +15,14 @@ char *str_concat(char *s1, char *s2)
 {
 	char *dest;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	dest = malloc((strlen(s1) + strlen(s2) + 1) * sizeof(char));
 	if (dest == NULL)
 		return (NULL);
-	dest = s1;
+	strcpy(dest, s1);
 	strcat(dest, s2);
 	return dest;
 }
