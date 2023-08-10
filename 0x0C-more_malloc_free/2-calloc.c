46aa2398f249043de3685c2672f6c233cf687229
SodimuDemilade <demisodimu@gmail.com>
2023-08-10 14:16:45 -0700

    malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,4 +9,19 @@
  *
  * @size: check
  *
- * Return: 
+ * Return: check
+ *
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *arr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	arr = malloc(size * nmemb);
+	if (arr == NULL)
+		return (NULL);
+	memset(arr, 0, nmemb * size);
+	return (arr);
+}
