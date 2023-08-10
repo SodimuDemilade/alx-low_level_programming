f38da4a6fa9e7e22600d505b4b31ea7f86feef72
SodimuDemilade <demisodimu@gmail.com>
2023-08-10 13:51:56 -0700

    malloc

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - main
+ *
+ * @s1: check
+ *
+ * @s2: check
+ *
+ * @n: check
+ *
+ * Return: check
+ *
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *comb;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	if (n >= strlen(s2))
+		n = strlen(s2);
+	comb = malloc((strlen(s1) + strlen(s2) + 1) * sizeof(char));
+	if (comb == NULL)
+		return (NULL);
+	strcpy(comb, s1);
+	strncat(comb, s2, n);
+	return (comb);
+}
