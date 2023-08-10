210ddf5d2c4e813207ca02ed24a34b31c8d627b0
SodimuDemilade <demisodimu@gmail.com>
2023-08-10 14:28:16 -0700

    malloc

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -20,7 +20,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	arr = malloc(sizeof(int) * (max - min + 2));
+	arr = malloc(sizeof(int) * (max - min + 1));
 	if (arr == NULL)
 		return (NULL);
 	for (i = 0 ; min <= max ; i++)
