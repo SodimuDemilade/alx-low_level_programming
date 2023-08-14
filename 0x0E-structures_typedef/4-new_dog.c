2ead718c822cd0b38ef08a144b0ab5223c3abe3f
SodimuDemilade <demisodimu@gmail.com>
2023-08-14 13:35:50 -0700

    typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,7 +20,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *mydog;
 
-	mydog = malloc(sizeof(dog_t));
+	mydog = (dog_t*)malloc(sizeof(dog_t));
 	if (mydog == NULL)
 		return (NULL);
 	mydog->name = strdup(name);
