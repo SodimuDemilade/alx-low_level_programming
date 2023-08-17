327d15a6b98fc23a7d6ef121d7dc0a456e461194
SodimuDemilade <demisodimu@gmail.com>
2023-08-17 08:15:23 -0700

    pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -37,13 +37,14 @@ void print_opcodes(int num_bytes)
 
 int main(int argc, char *argv[])
 {
-	int num_bytes = atoi(argv[1]);
-
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	int num_bytes = atoi(argv[1]);
+
 	if (num_bytes < 0)
 	{
 		printf("Error\n");
