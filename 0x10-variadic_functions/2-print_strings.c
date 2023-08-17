fa151d7988a9862cc2a308a8673824b580de8d4d
SodimuDemilade <demisodimu@gmail.com>
2023-08-17 10:19:47 -0700

    variadic

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - main
+ *
+ * @separator: check
+ *
+ * @n: check
+ *
+ * Return: check
+ *
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+	for (i = 0 ; i < n ; i++)
+	{
+		printf("%s", va_arg(ap, char*));
+		if (i == (n - 1))
+			printf("\n");
+		else
+			printf("%s", separator);
+	}
+}
