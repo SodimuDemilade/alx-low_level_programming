1ac688d278ed60ebf50f3d6e041e9f6f7e9c2650
SodimuDemilade <demisodimu@gmail.com>
2023-08-17 07:44:59 -0700

    pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,6 +17,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (array == NULL || cmp == NULL)
+		return (NULL);
 	if (size <= 0)
 		return (-1);
 	for (i = 0 ; i < size ; i++)
