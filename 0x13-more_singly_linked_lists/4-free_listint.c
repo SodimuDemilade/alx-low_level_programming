8e4455f49098d011db6726a293537f9d3fbc2cbe
SodimuDemilade <demisodimu@gmail.com>
2023-08-28 13:35:05 -0700

    linked lists

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #include "lists.h"
 
 /**
+ * free_listint - main
  *
+ * @head: input
+ *
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *current;
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}
