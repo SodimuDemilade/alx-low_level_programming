729ea739b063a1cbca4dca3f4149ca235150df99
SodimuDemilade <demisodimu@gmail.com>
2023-09-01 12:37:26 -0700

    bit manipulation

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * clear_bit - main
+ *
+ * @n: input
+ *
+ * @index: input
+ *
+ * Return: output
+ *
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask, one = 0;
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	mask = one << index;
+	*n &= mask;
+	return (1);
+}
