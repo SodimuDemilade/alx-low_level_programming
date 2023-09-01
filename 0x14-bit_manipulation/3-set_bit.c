90df4b32179e1fd64fa15eeddcbe8005e712668a
SodimuDemilade <demisodimu@gmail.com>
2023-09-01 12:13:24 -0700

    bit manipulation

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include <stdio,h>
+
+/**
+ * set_bit - main
+ *
+ * @n: input
+ *
+ * @index: input
+ *
+ * Return: output
+ *
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	
+}
