dd941785101cd872cec257c369b433f8cd05e3b3
SodimuDemilade <demisodimu@gmail.com>
2023-09-01 12:54:33 -0700

    bit manipulation

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * flip_bits - main
+ *
+ * @n: input
+ *
+ * @m: input
+ *
+ * Return: ouput
+ *
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int count = 0, result;
+
+	result = n ^ m;
+	while (result)
+	{
+		count += result & 1;
+		result  >>= 1;
+	}
+	return count;
+}
