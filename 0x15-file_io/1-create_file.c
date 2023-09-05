1a958bb1927ee6e56bebd6c322e571752efac641
SodimuDemilade <demisodimu@gmail.com>
2023-09-05 15:22:24 -0700

    files

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -37,5 +37,5 @@ int create_file(const char *filename, char *text_content)
 		}
 	}
 	close(fp);
-	return (1);	
+	return (1);
 }
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * append_text_to_file - main
+ *
+ * @filename: name of the file
+ *
+ * @text_content: content to write to file
+ *
+ * Return: output
+ *
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+
+}
